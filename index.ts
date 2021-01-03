46ee8c53318fb9cab1982348ed67b78daa11e3c8
Thomas1234567890 <tompatcon@gmail.com>
2021-01-03 22:22:15 +0000

    3 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,29 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+$(document).ready(function() {
+  $("#theme_switch").on("click", function() {
+    toggleTheme();
+  });
+
+  function toggleTheme() {
+    //switch between light and dark mode
+
+    //if ($('#theme_switch').prop('checked')) {
+    //    console.log('switch on');
+    //  }
+    //  else {
+    //    console.log('switch off');
+    //  }
+
+    $("body").toggleClass("dark-background");
+    $("#sub-title").toggleClass("dark-sub-title");
+    $("#input-field").toggleClass("dark-input-field");
+    $("#dark-switch-text").toggleClass("dark-switch-text");
+
+    $("body").toggleClass("light-background");
+    $("#sub-title").toggleClass("light-sub-title");
+    $("#input-field").toggleClass("light-input-field");
+    $("#light-switch-text").toggleClass("light-switch-text");
+  }
+});
